a1440b8b90c053af32192facf95c905476013a4c
jutelles <julianatelles.contato@gmail.com>
2020-09-06 17:55:18 -0300

    connection to front

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://grades-app-front-end.herokuapp.com/',
   })
 );
 
@@ -31,5 +31,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT || 8081, () => {
-  console.log('API em execucao');
+  console.log()
 });
